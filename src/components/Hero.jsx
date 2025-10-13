977f89a1edcec0551a28caecc50d5d59662db234
Dan <coolmax4700@gmail.com>
2025-10-13 18:26:06 +0300

    add Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import { Box, Flex, Button, HStack, Image, Link } from '@chakra-ui/react';
+import logo from '../assets/logo.svg';
+const Hero = () => {
+    return (
+        <Box bg="white" py={4} px={8} boxShadow="sm">
+            <Flex maxW="container.xl" mx="auto" justify="space-between" align="center">
+
+                <Link href="/" _hover={{ textDecoration: 'none' }}>
+                    <Image src={logo} alt="Логотип клуба" boxSize="70px" cursor="pointer" />
+                </Link>
+
+                <HStack spacing={6}>
+                    <Button variant="link" fontSize="lg" >Главная</Button>
+                    <Button variant="link" fontSize="lg" >О клубе</Button>
+                    <Button variant="link" fontSize="lg" >Цены</Button>
+                    <Button variant="link" fontSize="lg" >Галерея</Button>
+                    <Button variant="link" fontSize="lg" >Контакты</Button>
+                </HStack>
+                <Button bg="rgb(21, 128, 61)" color="white">Купить абонемент</Button>
+            </Flex>
+        </Box>
+    );
+}
+
+export default Hero;
